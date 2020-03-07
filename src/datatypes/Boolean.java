04228b7ea9998c248e7107db803ed0af2ee11fe6
Shubham Wagh <waghsgeeta@gmail.com>
2020-03-07 14:11:33 +0530

    first commit

---
+++ src/datatypes/Boolean.java
@@ -0,0 +1,9 @@
+package datatypes;
+
+public class Boolean {
+    public static void main(String[]args){
+        boolean a=false;
+        System.out.println(a);
+
+    }
+}
---
+++ src/datatypes/Int.java
@@ -0,0 +1,8 @@
+package datatypes;
+
+public class Int {
+    public static void main(String[]args){
+        int range=4250000;
+        System.out.println(range);
+    }
+}
